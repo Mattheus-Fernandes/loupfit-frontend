dbc23d742f9dec60eccd49c06ff018d647742d23
Mattheus Fernandes <Matt@mattfernandes.local>
2026-03-07 15:53:25 -0300

    refactor(dashboard): melhoria na seção da página de usuario

--- dashboard/src/app/core/models/interfaces/card.interface.ts
+++ dashboard/src/app/core/models/interfaces/card.interface.ts
@@ -1,6 +1,7 @@
 export interface ICard {
     icon: string,
     title: string,
+    text: string,
     value?: number
     redirectTo?: string,
     currencyCode: boolean
--- dashboard/src/app/shared/section-title/section-title.component.ts
+++ dashboard/src/app/shared/section-title/section-title.component.ts
@@ -7,4 +7,5 @@ import { Component, Input } from '@angular/core';
 })
 export class SectionTitleComponent {
   @Input() sectionTitle: string = ""
+  @Input() sectionText: string = ""
 }
