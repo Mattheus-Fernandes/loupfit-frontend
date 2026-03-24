fb365bc0718cf507dd4f34ccb291c78471908031
Mattheus Fernandes <mattheus.ale@hotmail.com>
2026-03-24 19:07:10 -0300

    refactor: atualizacao no tables-users

--- dashboard/src/app/shared/table-users/table-users.component.ts
+++ dashboard/src/app/shared/table-users/table-users.component.ts
@@ -1,6 +1,8 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { IUser } from 'src/app/core/models/interfaces/user.interface';
+import { TableColumn } from 'src/app/core/models/types/table-column.type';
 import { UsersResponse } from 'src/app/core/models/types/users-response';
+
 
 @Component({
   selector: 'app-table-users',
@@ -10,12 +12,36 @@ import { UsersResponse } from 'src/app/core/models/types/users-response';
 export class TableUsersComponent {
   @Input() usersList: UsersResponse = []
   @Input() thead: string[] = []
-  @Input() columnKey!: 'lastname' | 'role' | 'username'
+  @Input() columns!: TableColumn[]
   @Output() selectedUser = new EventEmitter<IUser>()
 
   onClick(user: IUser) {
     this.selectedUser.emit(user)
   }
 
+  definedColumns(colums: TableColumn[]): string {
+    const columnsSize = colums.length
 
+    return `repeat(${columnsSize}, 1fr)`
+  }
+
+  roleColor(role: string) {
+
+    switch (role) {
+      case "OWNER":
+        return "table__td--owner"
+
+      case "ADMIN":
+        return "table__td--admin"
+
+      case "EDITOR":
+        return "table__td--editor"
+
+      case "VIEWER":
+        return "table__td--viewer"
+
+      default:
+        return role;
+    }
+  }
 }
