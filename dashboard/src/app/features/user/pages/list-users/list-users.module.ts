ba57bed3e95f921730735272206a8b1c71cb67bc
Mattheus Fernandes <mattheus.ale@hotmail.com>
2026-03-24 21:02:20 -0300

    feat: trocado renderizacao de lista de usuario de table para card

--- dashboard/src/app/features/user/pages/list-users/list-users.module.ts
+++ dashboard/src/app/features/user/pages/list-users/list-users.module.ts
@@ -4,13 +4,11 @@ import { CommonModule } from '@angular/common';
 import { ListUsersRoutingModule } from './list-users-routing.module';
 import { ListUsersComponent } from './list-users.component';
 import { SharedModule } from 'src/app/shared/shared.module';
-import { ListUsersTableComponent } from './components/list-users-table/list-users-table.component';
 
 
 @NgModule({
   declarations: [
-    ListUsersComponent,
-    ListUsersTableComponent,
+    ListUsersComponent
   ],
   imports: [
     CommonModule,
