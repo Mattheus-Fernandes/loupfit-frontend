4ad915a1bd1b91de4bce5c958eb5dcf6e7c45504
Mattheus Fernandes <mattheus.ale@hotmail.com>
2026-03-29 10:19:52 -0300

    refactor(card-user): melhoria de UX e tornando o card clicavel

--- dashboard/src/app/shared/card-user/card-user.component.ts
+++ dashboard/src/app/shared/card-user/card-user.component.ts
@@ -16,20 +16,46 @@ export class CardUserComponent {
 
     switch (role) {
       case "OWNER":
-        return "card-user__role--owner"
+        return "card-user__access--owner"
 
       case "ADMIN":
-        return "card-user__role--admin"
+        return "card-user__access--admin"
 
       case "EDITOR":
-        return "card-user__role--editor"
+        return "card-user__access--editor"
 
       case "VIEWER":
-        return "card-user__role--viewer"
+        return "card-user__access--viewer"
 
       default:
         return role;
     }
+  }
+
+  roleIcon(role: string) {
+    switch (role) {
+      case "OWNER":
+        return "crown"
+
+      case "ADMIN":
+        return "admin_panel_settings"
+
+      case "EDITOR":
+        return "edit"
+
+      case "VIEWER":
+        return "visibility"
+
+      default:
+        return role;
+    }
+  }
+
+  abbreviationName(user: IUser): string {
+    const firstLetterName = user.name.charAt(0)
+    const firstLetterLastname = user.lastname.charAt(0)
+
+    return `${firstLetterName}${firstLetterLastname}`
   }
 
   btnOnClick() {
