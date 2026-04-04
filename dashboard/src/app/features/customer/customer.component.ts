c8f03dd24a6a2494d329823db83ac53cab256c09
Mattheus Fernandes <mattheus.ale@hotmail.com>
2026-04-04 18:43:11 -0300

    feat: adicionado pagina lista de clientes

--- dashboard/src/app/features/customer/customer.component.ts
+++ dashboard/src/app/features/customer/customer.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { CustomerService } from './services/customer.service';
+import { CustomerResponse } from './models/types/customer-response.types';
+import { take } from 'rxjs';
+import { ICustomer } from './models/interfaces/customer.interface';
 
 @Component({
   selector: 'app-customer',
   templateUrl: './customer.component.html',
   styleUrls: ['./customer.component.scss']
 })
-export class CustomerComponent {
+export class CustomerComponent implements OnInit{
+
+  private _customersList: CustomerResponse = []
+  private readonly _customerService = inject(CustomerService)
+  public customersListFiltered: CustomerResponse = []
+  public notFoundCustomer: string = ""
+
+  ngOnInit() {
+    this.getAllCustomers()
+  }
+
+  private getAllCustomers() {
+
+    this._customerService.getAllCustomers()
+      .pipe(
+        take(1)
+      )
+      .subscribe((res: CustomerResponse) => {
+        this._customersList = res
+        this.customersListFiltered = this._customersList
+      })
+  }
+
+  onSearch(value: string) {
+    const list = this.filterUsers(value)
+
+    if(list.length === 0) {
+      this.notFoundCustomer = "Nenhum cliente encontrado"
+    } 
+
+    this.customersListFiltered = list
+  }
+
+  private filterUsers(value: string) {
+    return this._customersList.filter((customer: ICustomer) => customer.name.toLowerCase().includes(value.toLowerCase()))
+  }
 
 }
--- dashboard/src/app/features/customer/customer.module.ts
+++ dashboard/src/app/features/customer/customer.module.ts
@@ -4,11 +4,21 @@ import { CommonModule } from '@angular/common';
 import { CustomerRoutingModule } from './customer-routing.module';
 import { CustomerComponent } from './customer.component';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { CustomerCardComponent } from './compenents/customer-card/customer-card.component';
+import { IconCardComponent } from './compenents/icon-card/icon-card.component';
+import { CpfPipe } from './pipes/cpf.pipe';
+import { PhonePipe } from './pipes/phone.pipe';
+import { GenderPipe } from './pipes/gender.pipe';
 
 
 @NgModule({
   declarations: [
-    CustomerComponent
+    CustomerComponent,
+    CustomerCardComponent,
+    IconCardComponent,
+    CpfPipe,
+    PhonePipe,
+    GenderPipe
   ],
   imports: [
     CommonModule,
