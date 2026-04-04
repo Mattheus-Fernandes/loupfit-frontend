2ef812fbb17e487d00aeefde0df9e6d554ca172c
Mattheus Fernandes <mattheus.ale@hotmail.com>
2026-04-04 18:41:05 -0300

    feat: criado interface do cliente e type para response

---
+++ dashboard/src/app/features/customer/models/interfaces/customer.interface.ts
@@ -0,0 +1,15 @@
+export interface ICustomer {
+  name: string
+  lastname: string
+  gender: string
+  username: string
+  password: string
+  email: string
+  phone: string
+  cpf: string
+  city: string
+  neighbour: string
+  street: string
+  number: number
+  uf: string
+}
