3600f5685ab3f957a4aa36f45302ae8ceb512632
Mattheus Fernandes <mattheus.ale@hotmail.com>
2026-04-06 20:00:37 -0300

    core: adicionado declaracao dos componentes AssetCard e AssetsList

--- dashboard/src/app/features/assets/assets.module.ts
+++ dashboard/src/app/features/assets/assets.module.ts
@@ -4,16 +4,23 @@ import { CommonModule } from '@angular/common';
 import { AssetsRoutingModule } from './assets-routing.module';
 import { AssetsComponent } from './assets.component';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { AssetsListComponent } from './components/assets-list/assets-list.component';
+import { AssetCardComponent } from './components/asset-card/asset-card.component';
 
 
 @NgModule({
   declarations: [
-    AssetsComponent
+    AssetsComponent,
+    AssetsListComponent,
+    AssetCardComponent
   ],
   imports: [
     CommonModule,
     AssetsRoutingModule,
     SharedModule
+  ],
+  exports: [
+    AssetsListComponent
   ]
 })
 export class AssetsModule { }
