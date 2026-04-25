ffd82cad5adc7bf2813775cd8806e4460e927dd9
Mattheus Fernandes <mattheus.ale@hotmail.com>
2026-04-25 11:45:50 -0300

    feat: cria configuracao de inputs para formulario de equipamentos

---
+++ dashboard/src/app/features/assets/config/edit-asset-inputs.config.ts
@@ -0,0 +1,48 @@
+import { IInput } from "src/app/core/models/interfaces/input.interface";
+
+export class EditAssetInputs {
+    static inputName(): IInput {
+        return {
+            icon: 'box_add',
+            name: 'name',
+            type: 'text',
+            placeholder: 'Nome'
+        }
+    }
+
+    static inputDescription(): IInput {
+        return {
+            icon: 'description',
+            name: 'description',
+            type: 'text',
+            placeholder: 'Descrição'
+        }
+    }
+
+    static inputQuantity(): IInput {
+        return {
+            icon: 'deployed_code',
+            name: 'quantity',
+            type: 'text',
+            placeholder: 'Quantidade'
+        }
+    }
+
+    static inputCostvalue(): IInput {
+        return {
+            icon: 'request_quote',
+            name: 'costValue',
+            type: 'text',
+            placeholder: 'Valor de custo'
+        }
+    }
+
+    static inputPlacePurchase(): IInput {
+        return {
+            icon: 'store',
+            name: 'placePurchase',
+            type: 'text',
+            placeholder: 'Local da compra'
+        }
+    }
+}
