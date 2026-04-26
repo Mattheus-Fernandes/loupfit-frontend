3b4d39b99d858c3d9a5c31903bdf24e384c64be2
Mattheus Fernandes <mattheus.ale@hotmail.com>
2026-04-26 16:35:36 -0300

    refactor(assets): declaracoes de componentes

--- dashboard/src/app/features/assets/assets.module.ts
+++ dashboard/src/app/features/assets/assets.module.ts
@@ -1,24 +1,33 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { AssetsRoutingModule } from './assets-routing.module';
 import { AssetsComponent } from './assets.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { AssetsListComponent } from './components/assets-list/assets-list.component';
 import { AssetCardComponent } from './components/asset-card/asset-card.component';
+import { EditAssetComponent } from './components/edit-asset/edit-asset.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { NewAssetComponent } from './components/new-asset/new-asset.component';
+import { SearchFilteredComponent } from './components/search-filtered/search-filtered.component';
+import { DeleteAssetComponent } from './components/delete-asset/delete-asset.component';
 
 
 @NgModule({
   declarations: [
     AssetsComponent,
     AssetsListComponent,
-    AssetCardComponent
+    AssetCardComponent,
+    EditAssetComponent,
+    NewAssetComponent,
+    SearchFilteredComponent,
+    DeleteAssetComponent
   ],
   imports: [
     CommonModule,
     AssetsRoutingModule,
-    SharedModule
-  ],
+    SharedModule,
+    ReactiveFormsModule
+],
   exports: [
     AssetsListComponent
   ]
